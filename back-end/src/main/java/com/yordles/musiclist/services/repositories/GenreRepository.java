af647df7d415a567106231945d9243d22fd753f3
Camilo Nossa <calejandro_noss@outlook.com>
2023-08-29 23:30:57 -0500

    Added @Repository decorator

--- back-end/src/main/java/com/yordles/musiclist/services/repositories/GenreRepository.java
+++ back-end/src/main/java/com/yordles/musiclist/services/repositories/GenreRepository.java
@@ -1,9 +1,11 @@
 package com.yordles.musiclist.services.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.yordles.musiclist.models.Genre;
 
+@Repository
 public interface GenreRepository extends CrudRepository<Genre, Long> {
 
 }
--- back-end/src/main/java/com/yordles/musiclist/services/repositories/SongRepository.java
+++ back-end/src/main/java/com/yordles/musiclist/services/repositories/SongRepository.java
@@ -1,9 +1,11 @@
 package com.yordles.musiclist.services.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.yordles.musiclist.models.Song;
 
+@Repository
 public interface SongRepository extends CrudRepository<Song, Long> {
     
 }

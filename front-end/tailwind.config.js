a4aa70820b96e9be8b6c122da2b06928214f8572
Camilo Nossa <calejandro_noss@outlook.com>
2023-09-03 04:45:42 -0500

    Added configuration to tailwind

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {},
+      fontSize: {
+        sm: "0.8rem",
+        base: "1rem",
+        lg: "1.15rem",
+        xl: "1.25rem",
+        "2xl": "1.563rem",
+        "3xl": "1.953rem",
+        "4xl": "2.441rem",
+        "5xl": "3.052rem",
+        "6xl": "3.5rem",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"]
+      },
+    },
   },
   plugins: [],
 };

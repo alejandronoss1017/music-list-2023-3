dbb105963e72aa2254e01a06484d8687994c3965
Camilo Nossa <calejandro_noss@outlook.com>
2023-09-09 18:39:13 -0500

    Added new class Admin

---
+++ back-end/src/main/java/com/yordles/musiclist/models/Admin.java
@@ -0,0 +1,22 @@
+package com.yordles.musiclist.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "admin")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+}

c568a4e4c709310fd149087dd95801bc64ee842b
Camilo Nossa <calejandro_noss@outlook.com>
2023-09-09 16:52:14 -0500

    Modified services according to new models

--- back-end/src/main/java/com/yordles/musiclist/services/SongService.java
+++ back-end/src/main/java/com/yordles/musiclist/services/SongService.java
@@ -1,52 +1,52 @@
-// package com.yordles.musiclist.services;
+package com.yordles.musiclist.services;
 
-// import com.yordles.musiclist.models.DTO.SongRequest;
-// import com.yordles.musiclist.models.Genre;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-// import com.yordles.musiclist.models.Song;
-// import com.yordles.musiclist.services.repositories.SongRepository;
+import com.yordles.musiclist.models.Song;
+import com.yordles.musiclist.services.repositories.SongRepository;
 
-// import java.util.Set;
+@Service
+public class SongService {
 
-// @Service
-// public class SongService {
+    @Autowired
+    private SongRepository songRepository;
 
-//     @Autowired
-//     private SongRepository songRepository;
+    public Iterable<Song> findAllSongs() {
+        return songRepository.findAll();
+    }
 
-//     public Iterable<Song> findAllSongs() {
-//         return songRepository.findAll();
-//     }
+    public Song findSongById(Long id) {
+        return songRepository.findById(id).orElse(null);
+    }
 
-//     public Song findSongById(Long id) {
-//         return songRepository.findById(id).orElse(null);
-//     }
+    public Song saveSong(Song song) {
+        return songRepository.save(song);
+    }
 
-//     public Song saveSong(Song song) {
-//         return songRepository.save(song);
-//     }
+    public Iterable<Song> saveManySongs(Iterable<Song> songs) {
+        return songRepository.saveAll(songs);
+    }
 
-//     public void deleteSongById(Long id) {
-//         songRepository.deleteById(id);
-//     }
+    public void deleteSongById(Long id) {
+        songRepository.deleteById(id);
+    }
 
-//     public void deleteSong(Song song) {
-//         songRepository.delete(song);
-//     }
+    public void deleteSong(Song song) {
+        songRepository.delete(song);
+    }
 
-//     public Song updateSong(Long id, SongRequest songRequest, Set<Genre> genres) {
-//         Song songToUpdate = findSongById(id);
+    // public Song updateSong(Long id, SongRequest songRequest, Set<Genre> genres) {
+    // Song songToUpdate = findSongById(id);
 
-//         songToUpdate.setName(songRequest.getName());
-//         songToUpdate.setArtist(songRequest.getArtist());
-//         songToUpdate.setAlbum(songRequest.getAlbum());
-//         songToUpdate.getGenres().clear();
-//         songToUpdate.getGenres().addAll(genres);
-//         songToUpdate.setReleaseDate(songRequest.getReleaseDate());
-//         songToUpdate.setDuration(songRequest.getDuration());
+    // songToUpdate.setName(songRequest.getName());
+    // songToUpdate.setArtist(songRequest.getArtist());
+    // songToUpdate.setAlbum(songRequest.getAlbum());
+    // songToUpdate.getGenres().clear();
+    // songToUpdate.getGenres().addAll(genres);
+    // songToUpdate.setReleaseDate(songRequest.getReleaseDate());
+    // songToUpdate.setDuration(songRequest.getDuration());
 
-//         return songRepository.save(songToUpdate);
-//     }
-// }
+    // return songRepository.save(songToUpdate);
+    // }
+}

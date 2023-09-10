3d8c3fc6adc74c1f228c540075773cb85489fa1b
Carlos Santiago Rojas Calderón <carlosantirc@gmail.com>
2023-09-10 01:25:47 -0500

    Service methods created in SongHasUserLike

--- back-end/src/main/java/com/yordles/musiclist/services/SongHasUserLikeService.java
+++ back-end/src/main/java/com/yordles/musiclist/services/SongHasUserLikeService.java
@@ -3,6 +3,9 @@ package com.yordles.musiclist.services;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.yordles.musiclist.models.Song;
+import com.yordles.musiclist.models.SongHasUserLike;
+import com.yordles.musiclist.models.User;
 import com.yordles.musiclist.models.EmbeddedKeys.SongHasUserLikeId;
 import com.yordles.musiclist.services.repositories.SongHasUserLikeRepository;
 
@@ -15,7 +18,31 @@ public class SongHasUserLikeService {
     private SongHasUserLikeRepository songHasUserLikeRepository;
 
     @Transactional
-    public void deleteUserById(SongHasUserLikeId id) {
+    public Iterable<SongHasUserLike> findAllSongHasUserLikes() {
+        return songHasUserLikeRepository.findAll();
+    }
+
+    @Transactional
+    public SongHasUserLike findSongHasUserLikeById(SongHasUserLikeId id) {
+        return songHasUserLikeRepository.findById(id).orElse(null);
+    }
+
+    @Transactional
+    public SongHasUserLike saveSongHasUserLike(SongHasUserLike id) {
+        return songHasUserLikeRepository.save(id);
+    }
+
+    @Transactional
+    public SongHasUserLike saveSongHasUserLike(Song song, User user) {
+        SongHasUserLikeId id = new SongHasUserLikeId(song.getId(), user.getId());
+        SongHasUserLike songHasUserLike = new SongHasUserLike(id, song, user);
+
+        return songHasUserLikeRepository.save(songHasUserLike);
+    }
+
+    @Transactional
+    public void deleteSongHasUserLikeById(Song song, User user) {
+        SongHasUserLikeId id = new SongHasUserLikeId(song.getId(), user.getId());
         songHasUserLikeRepository.deleteById(id);
     }
 }

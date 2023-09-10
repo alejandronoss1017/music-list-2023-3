ed9b74859d868538fac7a391bf5aff59de2f838d
Carlos Santiago Rojas Calderón <carlosantirc@gmail.com>
2023-09-10 01:23:27 -0500

    Added constructor with all the atributes in SonHasUserLike

--- back-end/src/main/java/com/yordles/musiclist/models/SongHasUserLike.java
+++ back-end/src/main/java/com/yordles/musiclist/models/SongHasUserLike.java
@@ -3,6 +3,9 @@ package com.yordles.musiclist.models;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
 import com.yordles.musiclist.models.EmbeddedKeys.SongHasUserLikeId;
 
 @Entity()
@@ -11,19 +14,24 @@ import com.yordles.musiclist.models.EmbeddedKeys.SongHasUserLikeId;
         @Index(name = "fk_song_has_user_like_user_id_idx", columnList = "user_id")
 })
 @Data
+@NonNull
+@RequiredArgsConstructor
 @NoArgsConstructor
 public class SongHasUserLike {
 
     @EmbeddedId
+    @NonNull
     private SongHasUserLikeId id;
 
     @ManyToOne
     @MapsId("songId")
+    @NonNull
     @JoinColumn(name = "song_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_song_has_user_like_song_id"))
     private Song song;
 
     @ManyToOne
     @MapsId("userId")
+    @NonNull
     @JoinColumn(name = "user_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_song_has_user_like_user_id"))
     private User user;
 

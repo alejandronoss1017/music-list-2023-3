51b61c9ce0e05e1c54c2977e894679970a6ed5f1
Carlos Santiago Rojas Calderón <carlosantirc@gmail.com>
2023-09-10 10:48:37 -0500

    Modified PlayListHasSong model

--- back-end/src/main/java/com/yordles/musiclist/models/PlayListHasSong.java
+++ back-end/src/main/java/com/yordles/musiclist/models/PlayListHasSong.java
@@ -1,23 +1,38 @@
 package com.yordles.musiclist.models;
 
+import com.yordles.musiclist.models.EmbeddedKeys.PlayListHasSongId;
+
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Entity()
 @Table(name = "playlist_has_song", indexes = {
         @Index(name = "fk_playlist_has_song_song_idx", columnList = "song_id"),
         @Index(name = "fk_playlist_has_song_playlist_idx", columnList = "playlist_id") })
 @Data
+@NonNull
+@RequiredArgsConstructor
 @NoArgsConstructor
 public class PlayListHasSong {
-    @Id
+
+    @EmbeddedId
+    @NonNull
+    private PlayListHasSongId id;
+
+
     @ManyToOne
+    @MapsId("playListId")
+    @NonNull
     @JoinColumn(name = "playlist_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_playlist_has_song_playlist_id"))
     PlayList playList;
 
-    @Id
+
     @ManyToOne
+    @MapsId("songId")
+    @NonNull
     @JoinColumn(name = "song_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_playlist_has_song_song_id"))
     Song song;
 }

127d2fd913e8820765bfdb419a6cb0aaf3a3aa58
Carlos Santiago Rojas Calderón <carlosantirc@gmail.com>
2023-10-10 01:26:33 -0500

    First routes

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationComponent } from './shared/pages/authentication/authentication.component';
+import { MainComponent } from './shared/pages/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AuthenticationComponent},
+  {path: 'main', component: MainComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

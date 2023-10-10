c2a7b50099c6642e1901e8a6a4c46dc83d82d9ff
Carlos Santiago Rojas Calderón <carlosantirc@gmail.com>
2023-10-10 01:28:00 -0500

    Added modules

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './components/button/button.component';
+import { ButtonComponent } from './shared/components/button/button.component';
+import { AuthenticationComponent } from './shared/pages/authentication/authentication.component';
+import { MainComponent } from './shared/pages/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonComponent,
+    AuthenticationComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,

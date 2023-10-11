ec3eba9294b082fafde4b895afca16a5f4a91938
Carlos Santiago Rojas Calderón <carlosantirc@gmail.com>
2023-10-11 17:32:09 -0500

    Added genre component

---
+++ front-end/src/app/shared/pages/genres/genres.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-genres',
+  templateUrl: './genres.component.html',
+  styleUrls: ['./genres.component.css']
+})
+export class GenresComponent {
+
+}

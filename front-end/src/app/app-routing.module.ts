4b8518b4cdc3c68fcbe9e04b677fdd36d42b4f66
Carlos Santiago Rojas Calderón <carlosantirc@gmail.com>
2023-10-11 17:34:45 -0500

    Added main pages routing

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationComponent } from './shared/pages/authentication/authentication.component';
 import { MainComponent } from './shared/pages/main/main.component';
+import { HomeComponent } from './shared/pages/home/home.component';
+import { GenresComponent } from './shared/pages/genres/genres.component';
 
 const routes: Routes = [
-  {path: '', component: AuthenticationComponent},
-  {path: 'main', component: MainComponent}
+  {path: 'authentication', component: AuthenticationComponent},
+  {path: 'main', component: MainComponent,
+    children:[
+       {path: '', component: HomeComponent},
+       {path: 'genre', component: GenresComponent}
+    ]},
+  {path: '**', redirectTo: '/authentication', pathMatch: 'full'}
 ];
 
 @NgModule({

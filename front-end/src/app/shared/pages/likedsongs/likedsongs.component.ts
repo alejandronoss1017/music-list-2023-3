3094ba13fe7dda82002988dc3f8851e764f17d5c
Carlos Santiago Rojas Calderón <carlosantirc@gmail.com>
2023-10-15 10:39:48 -0500

    Liked songs component creation

---
+++ front-end/src/app/shared/pages/likedsongs/likedsongs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-likedsongs',
+  templateUrl: './likedsongs.component.html',
+  styleUrls: ['./likedsongs.component.css']
+})
+export class LikedsongsComponent {
+
+}

f0e9b7e7af36597d8f3adb5700b4008f9e8ad909
Carlos Santiago Rojas Calderón <carlosantirc@gmail.com>
2023-10-16 00:44:58 -0500

    Added admin delete operation

---
+++ front-end/src/app/admin/pages/genre-d/genre-d.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-genre-d',
+  templateUrl: './genre-d.component.html',
+  styleUrls: ['./genre-d.component.css']
+})
+export class GenreDComponent {
+
+}

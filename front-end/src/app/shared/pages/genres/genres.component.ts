38237416b42afe951b92abd842bec3fc4245dee8
Carlos Santiago Rojas Calderón <carlosantirc@gmail.com>
2023-10-15 21:10:09 -0500

    Genre component optimization

--- front-end/src/app/shared/pages/genres/genres.component.ts
+++ front-end/src/app/shared/pages/genres/genres.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'shared-genres',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class GenresComponent {
 
+  genres: string[] = ['rock', 'pop', 'hiphop', 'electronic', 'metal', 'vallenato'];
+
+  constructor(private router: Router) {}
+
+  navigateToGenre(genre: string): void {
+    this.router.navigate(['/genre', genre]);
+  }
 }

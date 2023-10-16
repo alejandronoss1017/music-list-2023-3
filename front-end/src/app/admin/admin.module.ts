aa1f0ceadccf12779e0dc8c6f940fecdbc46e7a0
Carlos Santiago Rojas Calderón <carlosantirc@gmail.com>
2023-10-16 00:43:16 -0500

    Updated admin module

--- front-end/src/app/admin/admin.module.ts
+++ front-end/src/app/admin/admin.module.ts
@@ -6,6 +6,15 @@ import { AdminSongComponent } from './pages/admin-song/admin-song.component';
 import { AdminSidebarComponent } from './components/admin-sidebar/admin-sidebar.component';
 import { SharedModule } from "../shared/shared.module";
 import { RouterModule } from '@angular/router';
+import { GenreCComponent } from './pages/genre-c/genre-c.component';
+import { GenreDComponent } from './pages/genre-d/genre-d.component';
+import { GenreRComponent } from './pages/genre-r/genre-r.component';
+import { GenreUComponent } from './pages/genre-u/genre-u.component';
+import { SongCComponent } from './pages/song-c/song-c.component';
+import { SongUComponent } from './pages/song-u/song-u.component';
+import { SongRComponent } from './pages/song-r/song-r.component';
+import { SongDComponent } from './pages/song-d/song-d.component';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 
 @NgModule({
@@ -14,11 +23,23 @@ import { RouterModule } from '@angular/router';
         AdminGenreComponent,
         AdminSongComponent,
         AdminSidebarComponent,
+        GenreCComponent,
+        GenreDComponent,
+        GenreRComponent,
+        GenreUComponent,
+        SongCComponent,
+        SongUComponent,
+        SongRComponent,
+        SongDComponent,
     ],
     imports: [
         CommonModule,
         SharedModule,
-        RouterModule
+        RouterModule,
+        FormsModule
+    ],
+    exports: [
+        AdminSidebarComponent
     ]
 })
 export class AdminModule { }

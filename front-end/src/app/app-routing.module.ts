515a84d671fe4624dbd3e542172a61e7e340035a
Carlos Santiago Rojas Calderón <carlosantirc@gmail.com>
2023-10-16 00:42:59 -0500

    Added admin to routing

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -11,6 +11,14 @@ import { SignUpComponent } from './shared/pages/sign-up/sign-up.component';
 import { AdminMainPageComponent } from './admin/pages/admin-main-page/admin-main-page.component';
 import { AdminGenreComponent } from './admin/pages/admin-genre/admin-genre.component';
 import { AdminSongComponent } from './admin/pages/admin-song/admin-song.component';
+import { GenreCComponent } from './admin/pages/genre-c/genre-c.component';
+import { GenreRComponent } from './admin/pages/genre-r/genre-r.component';
+import { GenreUComponent } from './admin/pages/genre-u/genre-u.component';
+import { SongCComponent } from './admin/pages/song-c/song-c.component';
+import { SongRComponent } from './admin/pages/song-r/song-r.component';
+import { SongUComponent } from './admin/pages/song-u/song-u.component';
+import { SongDComponent } from './admin/pages/song-d/song-d.component';
+import { GenreDComponent } from './admin/pages/genre-d/genre-d.component';
 
 const routes: Routes = [
   {path: 'authentication', component: AuthenticationComponent},
@@ -28,9 +36,28 @@ const routes: Routes = [
     ]},
   {path: 'admin', component: AdminMainPageComponent,
     children:[
-        {path: 'crudgenres', component: AdminGenreComponent},
-        {path: 'crudsongs', component: AdminSongComponent}
-    ]},
+      {path: 'crudgenres', component: AdminGenreComponent,
+        children: 
+          [
+            {path: '', component: GenreCComponent},
+            {path: 'create', component: GenreCComponent},
+            {path: 'read', component: GenreRComponent},
+            {path: 'update', component: GenreUComponent},
+            {path: 'delete', component: GenreDComponent},
+          ]
+      },
+      {path: 'crudsongs', component: AdminSongComponent,
+        children: 
+          [
+            {path: '', component: SongCComponent},
+            {path: 'create', component: SongCComponent},
+            {path: 'read', component: SongRComponent},
+            {path: 'update', component: SongUComponent},
+            {path: 'delete', component: SongDComponent},
+          ]          
+        }
+      ]
+  },
   {path: '**', redirectTo: '/authentication', pathMatch: 'full'},
 ];
 

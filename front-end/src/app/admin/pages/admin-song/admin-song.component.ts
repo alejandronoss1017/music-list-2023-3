a95bfc80a5c7957e00408ba032c87eb50055a24e
Camilo Nossa <calejandro_noss@outlook.com>
2023-10-21 17:24:39 -0500

    Refactor admin-song page component

--- front-end/src/app/admin/pages/admin-song/admin-song.component.ts
+++ front-end/src/app/admin/pages/admin-song/admin-song.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-admin-song',
   templateUrl: './admin-song.component.html',
-  styleUrls: ['./admin-song.component.css']
+  styleUrls: ['./admin-song.component.css'],
 })
 export class AdminSongComponent {
+  isModalOpen: boolean = false;
 
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
 }
+

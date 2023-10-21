6703466943010a6a0283c90556583af4dd7f67ba
Camilo Nossa <calejandro_noss@outlook.com>
2023-10-21 17:14:10 -0500

    Refactor admin-sidebar component

--- front-end/src/app/admin/components/admin-sidebar/admin-sidebar.component.ts
+++ front-end/src/app/admin/components/admin-sidebar/admin-sidebar.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-sidebar',
   templateUrl: './admin-sidebar.component.html',
-  styleUrls: ['./admin-sidebar.component.css']
+  styleUrls: ['./admin-sidebar.component.css'],
 })
 export class AdminSidebarComponent {
+  constructor(private router: Router) {}
 
+  isCurrentRoute(route: string): boolean {
+    return this.router.url === route;
+  }
 }

44a4fe85d7a114506e1e7ada5ac132b48ab54f9f
Camilo Nossa <calejandro_noss@outlook.com>
2023-10-21 17:27:59 -0500

    Added new routes to admin

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -11,14 +11,10 @@ import { SignUpComponent } from './shared/pages/sign-up/sign-up.component';
 import { AdminMainPageComponent } from './admin/pages/admin-main-page/admin-main-page.component';
 import { AdminGenreComponent } from './admin/pages/admin-genre/admin-genre.component';
 import { AdminSongComponent } from './admin/pages/admin-song/admin-song.component';
-import { GenreCComponent } from './admin/pages/genre-c/genre-c.component';
-import { GenreRComponent } from './admin/pages/genre-r/genre-r.component';
-import { GenreUComponent } from './admin/pages/genre-u/genre-u.component';
-import { SongCComponent } from './admin/pages/song-c/song-c.component';
-import { SongRComponent } from './admin/pages/song-r/song-r.component';
-import { SongUComponent } from './admin/pages/song-u/song-u.component';
-import { SongDComponent } from './admin/pages/song-d/song-d.component';
-import { GenreDComponent } from './admin/pages/genre-d/genre-d.component';
+import { AdminUpdateSongComponent } from './admin/pages/admin-update-song/admin-update-song.component';
+import { AdminCreateSongComponent } from './admin/pages/admin-create-song/admin-create-song.component';
+import { AdminUpdateGenreComponent } from './admin/pages/admin-update-genre/admin-update-genre.component';
+import { AdminCreateGenreComponent } from './admin/pages/admin-create-genre/admin-create-genre.component';
 
 const routes: Routes = [
   { path: 'authentication', component: AuthenticationComponent },
@@ -42,25 +38,19 @@ const routes: Routes = [
     component: AdminMainPageComponent,
     children: [
       {
-        path: 'crudgenres',
-        component: AdminGenreComponent,
+        path: '',
+        component: AdminSongComponent,
         children: [
-          { path: '', component: GenreCComponent },
-          { path: 'create', component: GenreCComponent },
-          { path: 'read', component: GenreRComponent },
-          { path: 'update', component: GenreUComponent },
-          { path: 'delete', component: GenreDComponent },
+          { path: 'update/:id', component: AdminUpdateSongComponent },
+          { path: 'create', component: AdminCreateSongComponent },
         ],
       },
       {
-        path: 'crudsongs',
-        component: AdminSongComponent,
+        path: 'genres',
+        component: AdminGenreComponent,
         children: [
-          { path: '', component: SongCComponent },
-          { path: 'create', component: SongCComponent },
-          { path: 'read', component: SongRComponent },
-          { path: 'update', component: SongUComponent },
-          { path: 'delete', component: SongDComponent },
+          { path: 'update/:id', component: AdminUpdateGenreComponent },
+          { path: 'create', component: AdminCreateGenreComponent },
         ],
       },
     ],

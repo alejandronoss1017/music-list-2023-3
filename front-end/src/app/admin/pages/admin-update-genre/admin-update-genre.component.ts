0f914a1df955658f97089a97482b51d157398062
Camilo Nossa <calejandro_noss@outlook.com>
2023-10-21 17:22:47 -0500

    Created new admin-update-genre page component

---
+++ front-end/src/app/admin/pages/admin-update-genre/admin-update-genre.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-update-genre',
+  templateUrl: './admin-update-genre.component.html',
+  styleUrls: ['./admin-update-genre.component.css']
+})
+export class AdminUpdateGenreComponent {
+
+}

42606812813bed90922c8c32f954039015f84759
Camilo Nossa <calejandro_noss@outlook.com>
2023-10-21 17:23:26 -0500

    Created new admin-create-genre page component

---
+++ front-end/src/app/admin/pages/admin-create-genre/admin-create-genre.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-create-genre',
+  templateUrl: './admin-create-genre.component.html',
+  styleUrls: ['./admin-create-genre.component.css']
+})
+export class AdminCreateGenreComponent {
+
+}

b5ea13800ef8a5c70c328691dd7cd49227916a16
Camilo Nossa <calejandro_noss@outlook.com>
2023-10-21 02:33:20 -0500

    Refactor genre component

--- front-end/src/app/shared/pages/genres/genres.component.ts
+++ front-end/src/app/shared/pages/genres/genres.component.ts
@@ -4,15 +4,42 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'shared-genres',
   templateUrl: './genres.component.html',
-  styleUrls: ['./genres.component.css']
+  styleUrls: ['./genres.component.css'],
 })
 export class GenresComponent {
-
-  genres: string[] = ['rock', 'pop', 'hiphop', 'electronic', 'metal', 'vallenato'];
+  genres: string[] = [
+    'rock',
+    'pop',
+    'hiphop',
+    'electronic',
+    'metal',
+    'vallenato',
+    'reggaeton',
+    'trap',
+    'salsa',
+    'ranchera',
+    'cumbia',
+    'merengue',
+    'bachata',
+    'balada',
+    'bolero',
+    'tango',
+    'jazz',
+    'blues',
+    'soul',
+    'funk',
+    'country',
+    'reggae',
+    'ska',
+    'punk',
+    'indie',
+    'alternativo',
+    'clásica',
+  ];
 
   constructor(private router: Router) {}
 
-  navigateToGenre(genre: string): void {
-    this.router.navigate(['/genre', genre]);
+  navigateToGenre(genreOMG: string): void {
+    this.router.navigate(['/genre', genreOMG]);
   }
 }

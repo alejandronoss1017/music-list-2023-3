3cf6f134ba02bef74ce7b3c717c8361381c7e298
Carlos Santiago Rojas Calderón <carlosantirc@gmail.com>
2023-11-04 00:19:10 -0500

    Added the song service in search component

--- front-end/src/app/shared/pages/search/search.component.ts
+++ front-end/src/app/shared/pages/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Song } from 'src/types/Song';
+import { SongService } from '../../services/songs/song.service';
 
 @Component({
   selector: 'app-search',
@@ -7,32 +9,18 @@ import { Component } from '@angular/core';
 })
 export class SearchComponent {
 
-  musicList = [
-  { name: 'Cancion1', artist: 'Artist Name 1', duration: '4:20', album: 'Album Name 1' },
-  { name: 'Song name 1', artist: 'Artist Name 1', duration: '4:20', album: 'Album Name 1' },
-  { name: 'Song name 2', artist: 'Artist Name 2', duration: '3:45', album: 'Album Name 2' },
-  { name: 'Another Song', artist: 'Another Artist', duration: '3:15', album: 'Another Album' },
-  { name: 'Hit Song', artist: 'Famous Artist', duration: '2:50', album: 'Greatest Hits' },
-  { name: 'Cool Tune', artist: 'Cool Artist', duration: '3:10', album: 'Cool Album' },
-  { name: 'Jazz Groove', artist: 'Jazz Ensemble', duration: '5:30', album: 'Smooth Jazz' },
-  { name: 'Rock Anthem', artist: 'Rock Band', duration: '4:15', album: 'Rock Classics' },
-  { name: 'Dance Beat', artist: 'DJ Mixmaster', duration: '4:00', album: 'Club Hits' },
-  { name: 'Country Ballad', artist: 'Country Singer', duration: '3:55', album: 'Country Stories' },
-  { name: 'Rap Rhymes', artist: 'Rapper MC', duration: '3:30', album: 'Hip-Hop Evolution' },
-  ];
+  musicList: Song[] = [];
 
-filteredMusicList: Array<{ name: string; artist: string; duration: string; album: string }> = [];
+  constructor(private songService: SongService) {}
 
   searchSong(term: string): void {
     console.log('Searching for songs with term:', term);
-    this.filteredMusicList = this.musicList.filter((musicItem) =>
-      musicItem.name.toLowerCase().includes(term.toLowerCase())
-    );
+    this.songService.getSongByName(term).subscribe((songs: any) => {
+      this.musicList = songs;
+    });
   }
 
-  favoriteStates: boolean[] = new Array(this.musicList.length).fill(false);
-
-  toggleHeartColor(index: number) {
-    this.favoriteStates[index] = !this.favoriteStates[index];
+  toggleHeartColor(song: Song) {
+    song.favorite = !song.favorite;
   }
 }

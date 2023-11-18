de15476c57e7362ebf9e7e631b502a1f15504037
Camilo Nossa <calejandro_noss@outlook.com>
2023-11-17 20:25:55 -0500

    Implemented on submit method

--- front-end/src/app/shared/pages/sign-up/sign-up.component.ts
+++ front-end/src/app/shared/pages/sign-up/sign-up.component.ts
@@ -1,6 +1,8 @@
+import { AuthService } from './../../services/auth/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { RegisterRequest } from 'src/types/RegisterRequest';
 
 @Component({
   selector: 'app-sign-up',
@@ -11,10 +13,12 @@ export class SignUpComponent implements OnInit {
   // Define the form group
   registerForm: FormGroup;
 
-
-  // TODO: Inject the users service
   // Inject the FormBuilder
-  constructor(private fb: FormBuilder, private router: Router) {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) {
     // Create the form
     this.registerForm = this.fb.group({
       username: ['', Validators.required],
@@ -22,14 +26,14 @@ export class SignUpComponent implements OnInit {
       password: ['', Validators.required],
     });
   }
+
   ngOnInit(): void {
     // TODO: Implement users service to create a user
   }
 
   onSubmit() {
-    // TODO: Call the users service to create a user
+    this.authService.register(this.registerForm.value as RegisterRequest);
 
     // Change page to home
-    this.router.navigate(['/main']);
   }
 }

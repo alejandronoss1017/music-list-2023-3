ef79f004dc0aae9f86a36e3bdace7c6eb7c34653
Camilo Nossa <calejandro_noss@outlook.com>
2023-11-17 19:02:38 -0500

    Fixed type error

--- JWT/src/main/java/com/yordles/jwt/controllers/AuthenticationController.java
+++ JWT/src/main/java/com/yordles/jwt/controllers/AuthenticationController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(value = "*")
+@CrossOrigin(origins = "*")
 public class AuthenticationController {
 
     @Autowired

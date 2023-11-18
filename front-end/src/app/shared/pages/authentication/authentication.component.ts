4991a752f0b7b14f7a33ee3dfeed9b19e8975676
Camilo Nossa <calejandro_noss@outlook.com>
2023-11-17 20:25:36 -0500

    Added form to make the request

--- front-end/src/app/shared/pages/authentication/authentication.component.ts
+++ front-end/src/app/shared/pages/authentication/authentication.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
+import { LoginRequest } from 'src/types/LoginRequest';
 
 @Component({
   selector: 'app-authentication',
   templateUrl: './authentication.component.html',
-  styleUrls: ['./authentication.component.css']
+  styleUrls: ['./authentication.component.css'],
 })
-export class AuthenticationComponent {
+export class AuthenticationComponent implements OnInit {
+  // Define the form group
+  authForm: FormGroup;
 
+  ngOnInit(): void {}
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.authForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    this.authService.login(this.authForm.value as LoginRequest);
+  }
 }
